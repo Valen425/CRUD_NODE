3aa9386814c4ca515c7118e62fadc1d8eac67849
Valen425 <cuevacespedesvalentino@gmail.com>
2026-04-27 12:06:46 -0500

    Mi primera pagina

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const app =express();
 app.get('/',(req,res)=>{
-    res.send('Hola mundo');
+    res.send('Mi primera pagina');
 });
 app.listen(3000, ()=>{
     console.log('servidor corriendo en http://localhost:3000');
